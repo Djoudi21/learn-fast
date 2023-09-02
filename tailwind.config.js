a24006bb6c3ddcd4b299ccf44146c2cd2a781465
Djoudi21 <abdelkrim@gdpr.dev>
2023-09-02 17:27:59 +0200

    feat: Add new color palette to theme
    - Added new colors to the theme configuration in tailwind.config.js
    - New colors include primary, secondary, silver, bright, volcanicSand, payneGrey, lightGray, label, bInput, error, brightGray, greyRelief and veryLightGrey

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#212121',
+        secondary: '#6e7682',
+        silver: '#c1c1c1',
+        bright: '#fdfdfd',
+        volcanicSand: '#3E3E44',
+        payneGrey: '#3C3C40',
+        lightGray: '#59606A',
+        label: '#6e7682',
+        bInput: '#e4e4e4',
+        error: '#cb0505',
+        brightGray: '#E8ECF1',
+        greyRelief: '#E4E8EC',
+        veryLightGrey: '#E8ECF0',
+      },
+    },
   },
   plugins: [],
 };
