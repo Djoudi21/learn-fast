7506e77a820574924ebc02afed48f95f750474e7
Djoudi21 <abdelkrim@gdpr.dev>
2023-09-03 01:25:58 +0200

    feat(auth): implement login functionality
    - Removed unnecessary console.log statement in login async thunk
    - Implemented the login functionality using the InMemoryAuthRepository and LoginUseCase classes

--- src/store/auth/login.ts
+++ src/store/auth/login.ts
@@ -4,7 +4,6 @@ import {InMemoryAuthRepository} from '../../repositories/inMemoryAuthRepository'
 import {User} from '../../use-cases/loginUseCase/types';
 
 export const login = createAsyncThunk('auth/logout', async (user: User) => {
-  console.log('login async thunk');
   const inMemoryAuthRepository = new InMemoryAuthRepository();
   const loginUseCase = new LoginUseCase(inMemoryAuthRepository);
   return await loginUseCase.login(user);
