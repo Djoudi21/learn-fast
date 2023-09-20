a962ef738df1d7aa45a8dad1cd07e8a1a5e86a37
Djoudi21 <abdelkrim@gdpr.dev>
2023-09-20 02:17:38 +0200

    wip

--- src/screens/ConversationDetails.tsx
+++ src/screens/ConversationDetails.tsx
@@ -1,14 +1,15 @@
-import {FlatList, SafeAreaView} from 'react-native';
-import React, {useEffect} from 'react';
+import {FlatList, SafeAreaView, TextInput} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {listMessagesByConversationId} from '../store/messages/listMessagesByConversationId';
 import {MessageListItem} from '../components/MessageListItem';
 import {Message} from '../use-cases/listMessagesUseCase/types';
+import {BaseTextInput} from '../components/atomics/BaseTextInput';
 
 export function ConversationDetails({route}) {
+  const [userInput, setUserInput] = useState('');
   const {conversationId} = route.params;
   const messages = useSelector((state: any) => state.messages.messages);
-  console.log(messages);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(listMessagesByConversationId(conversationId));
@@ -18,11 +19,27 @@ export function ConversationDetails({route}) {
     return <MessageListItem message={message} />;
   };
   return (
-    <SafeAreaView className={'bg-[#FDFDFD] h-screen'}>
+    <SafeAreaView className={'bg-[#FDFDFD]'}>
       <FlatList
+        className={'border border-solid border-error'}
         data={messages}
         renderItem={({item}) => renderItem(item)}
         keyExtractor={item => item.id}
+      />
+      <BaseTextInput
+        // onFocus={}
+        // onBlur={}
+        placeholder="Saissez votre message"
+        // handlePressIcon={() =>
+        //   auth.setIsSecureTextEntry(!auth.iSecureTextEntry)
+        // }
+        // renderIcon={renderIconEye}
+        onChangeText={setUserInput}
+        value={userInput}
+        // secureTextEntry={auth.iSecureTextEntry}
+        className={'w-11/12 h-full'}
+        // errorMessage={auth.passwordErrorMessage}
+        containerStyle={'border border-error border-solid h-10'}
       />
     </SafeAreaView>
   );
